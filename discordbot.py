856c2a746ddc63cb5fb6c154e724066ce92e9cf9
Reki Yamashita <chloir.sharp@gmail.com>
2019-09-01 20:15:18 +0900

    Add Random Roll

--- discordbot.py
+++ discordbot.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import os
+import random
 import traceback
 
 bot = commands.Bot(command_prefix='/')
@@ -18,7 +19,21 @@ async def ping(ctx):
 
 @bot.command()
 async def roll(ctx):
-    await ctx.send('Random Role')
+    roll_index = random.randint(0, 1000)
+    rolled_role = 'none'
+
+    if roll_index % 5 == 0:
+        rolled_role = 'Top'
+    elif roll_index % 5 == 1:
+        rolled_role = 'Jg'
+    elif roll_index % 5 == 2:
+        rolled_role = 'Mid'
+    elif roll_index % 5 == 3:
+        rolled_role = 'Adc'
+    elif roll_index % 5 == 4:
+        rolled_role = 'Sup'
+
+    await ctx.send(rolled_role)
 
 
 bot.run(token)
