07e3908819cc1eb7ed30d7cf1e62c69477686cc1
Reki Yamashita <chloir.sharp@gmail.com>
2019-09-02 01:41:07 +0900

    Add help "/role" command can also used as random roll command

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,7 @@ import traceback
 
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+help_text = '/roll,/role:ロールを決める'
 
 '''
 @bot.event
@@ -20,21 +21,40 @@ async def ping(ctx):
 
 @bot.command()
 async def roll(ctx):
+    rolled_role = role_roller()
+    await ctx.send(rolled_role)
+
+
+@bot.command()
+async def role(ctx):
+    rolled_role = role_roller()
+    await ctx.send(rolled_role)
+
+
+@bot.command()
+async def roller_help(ctx):
+    await ctx.send(help_text)
+
+
+def role_roller():
     roll_index = random.randint(0, 1000)
     rolled_role = 'none'
 
     if roll_index % 5 == 0:
         rolled_role = 'Top'
+        return rolled_role
     elif roll_index % 5 == 1:
         rolled_role = 'Jg'
+        return rolled_role
     elif roll_index % 5 == 2:
         rolled_role = 'Mid'
+        return rolled_role
     elif roll_index % 5 == 3:
         rolled_role = 'Adc'
+        return rolled_role
     elif roll_index % 5 == 4:
         rolled_role = 'Sup'
-
-    await ctx.send(rolled_role)
+        return rolled_role
 
 
 bot.run(token)
